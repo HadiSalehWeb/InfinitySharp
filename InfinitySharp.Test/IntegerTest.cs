7bcaa1c50c8a0d36112c2e06534f77d76fa01629
Hadi Saleh <Hadi.saleh852@outlook.com>
2018-11-30 11:19:33 +0100

    Reorganization

---
+++ InfinitySharp.Test/IntegerTest.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InfinitySharp;
+
+namespace InfinitySharp.Test
+{
+    [TestClass]
+    public class IntegerTest
+    {
+        private readonly Integer zero, one, two, ten, thou, inf, nan;
+
+        public IntegerTest()
+        {
+            zero = 0;
+            one = 1;
+            two = 2;
+            ten = 10;
+            thou = 1000;
+            inf = Integer.Infinity;
+            nan = Integer.NaN;
+            Integer.onOverflow = OnIntegerOverflow.Throw;
+        }
+
+        [TestMethod]
+        public void TestAddition()
+        {
+            Assert.AreEqual(zero + one, one);
+            Assert.AreEqual(one + one, two);
+            Assert.AreEqual(zero + zero, zero);
+        }
+
+        [TestMethod]
+        public void TestSubstraction()
+        {
+            Assert.AreEqual(two - one, one);
+            Assert.AreEqual(one - zero, one);
+            Assert.AreEqual(one - one, zero);
+            Assert.AreEqual(zero - zero, zero);
+        }
+
+        [TestMethod]
+        public void TestNegation()
+        {
+            Assert.AreEqual(-zero, zero);
+            Assert.AreEqual(-one, -1);
+            Assert.AreEqual(-thou, -1000);
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Assert.AreEqual(one * one, one);
+            Assert.AreEqual(thou * zero, zero);
+            Assert.AreEqual(ten * ten * ten, thou);
+            Assert.AreEqual(two * one, two);
+            Assert.AreEqual(-one * one, -one);
+            Assert.AreEqual(-two * -one, two);
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            Assert.AreEqual(one / one, one);
+            Assert.AreEqual(one / two, zero);
+            Assert.AreEqual(zero / thou, zero);
+            Assert.AreEqual(thou / (ten * ten), ten);
+            Assert.AreEqual(zero / -one, zero);
+            Assert.AreEqual(-zero / ten, zero);
+            Assert.AreEqual(-ten / -one, ten);
+            Assert.AreEqual(one / zero, inf);
+        }
+
+        [TestMethod]
+        public void TestPower()
+        {
+            Assert.AreEqual(Integer.Pow(ten, two + one), thou);
+            Assert.AreEqual(Integer.Pow(two, zero), one);
+            Assert.AreEqual(Integer.Pow(ten, one), ten);
+            Assert.AreEqual(Integer.Pow(one, thou), one);
+        }
+
+        [TestMethod]
+        public void TestInfinity_add_sub()
+        {
+            Assert.AreEqual(inf + one, inf);
+            Assert.AreEqual(inf - ten, inf);
+            Assert.AreEqual(-inf + two, -inf);
+            Assert.AreEqual(-inf - thou, -inf);
+            Assert.AreEqual(inf + -one, inf);
+            Assert.AreEqual(inf - -ten, inf);
+            Assert.AreEqual(-inf + -two, -inf);
+            Assert.AreEqual(-inf - -thou, -inf);
+            Assert.AreEqual(inf + inf, inf);
+            Assert.AreEqual(-inf + -inf, -inf);
+            Assert.AreEqual(inf - inf, nan);
+        }
+
+        [TestMethod]
+        public void TestInfinity_mult()
+        {
+            Assert.AreEqual(inf * one, inf);
+            Assert.AreEqual(inf * -two, -inf);
+            Assert.AreEqual(-inf * ten, -inf);
+            Assert.AreEqual(-inf * -thou, inf);
+            Assert.AreEqual(inf * inf, inf);
+            Assert.AreEqual(inf * -inf, -inf);
+            Assert.AreEqual(-inf * -inf, inf);
+            Assert.AreEqual(zero * -inf, nan);
+        }
+
+        [TestMethod]
+        public void TestInfinity_div()
+        {
+            Assert.AreEqual(two / zero, inf);
+            Assert.AreEqual(-ten / zero, -inf);
+            Assert.AreEqual(one / inf, zero);
+            Assert.AreEqual(-two / inf, zero);
+            Assert.AreEqual(ten / -inf, zero);
+            Assert.AreEqual(-thou / -inf, zero);
+            Assert.AreEqual(inf / one, inf);
+            Assert.AreEqual(inf / -two, -inf);
+            Assert.AreEqual(-inf / ten, -inf);
+            Assert.AreEqual(-inf / -thou, inf);
+            Assert.AreEqual(zero / inf, zero);
+            Assert.AreEqual(zero / -inf, zero);
+            Assert.AreEqual(inf / zero, inf);
+            Assert.AreEqual(-inf / zero, -inf);
+            Assert.AreEqual(zero / zero, nan);
+            Assert.AreEqual(inf / inf, nan);
+            Assert.AreEqual(inf / -inf, nan);
+            Assert.AreEqual(-inf / inf, nan);
+            Assert.AreEqual(-inf / -inf, nan);
+        }
+
+        [TestMethod]
+        public void TestInfinity_pow()
+        {
+            Assert.AreEqual(Integer.Pow(one, zero), 1);
+            Assert.AreEqual(Integer.Pow(-ten, zero), 1);
+            Assert.AreEqual(Integer.Pow(thou, zero), 1);
+            Assert.AreEqual(Integer.Pow(zero, zero), nan);
+            Assert.AreEqual(Integer.Pow(inf, zero), nan);
+            Assert.AreEqual(Integer.Pow(zero, one), zero);
+            Assert.AreEqual(Integer.Pow(zero, -two), inf);
+            Assert.AreEqual(Integer.Pow(two, inf), inf);
+            Assert.AreEqual(Integer.Pow(one, inf), nan);
+            Assert.AreEqual(Integer.Pow(zero, inf), zero);
+            Assert.AreEqual(Integer.Pow(-ten, inf), -inf);
+            Assert.AreEqual(Integer.Pow(two, -inf), zero);
+            Assert.AreEqual(Integer.Pow(one, -inf), nan);
+            Assert.AreEqual(Integer.Pow(zero, -inf), inf);
+            Assert.AreEqual(Integer.Pow(-ten, -inf), zero);
+            Assert.AreEqual(Integer.Pow(inf, inf), inf);
+        }
+
+        [TestMethod]
+        public void TestGetHashCode()
+        {
+            Assert.AreEqual(zero.GetHashCode(), 0);
+            Assert.AreEqual(one.GetHashCode(), 1);
+            Assert.AreEqual(thou.GetHashCode(), 1000);
+            Assert.AreEqual(inf.GetHashCode(), int.MaxValue);
+            Assert.AreEqual((-inf).GetHashCode(), int.MinValue);
+            Assert.AreEqual(nan.GetHashCode(), 0);
+        }
+
+        [TestMethod]
+        public void TestEquals()
+        {
+            Assert.AreEqual(zero == 0, true);
+            Assert.AreEqual(one == 1, true);
+            Assert.AreEqual(-thou == -1000, true);
+            Assert.AreEqual(inf == Integer.Infinity, true);
+            Assert.AreEqual(-inf == -Integer.Infinity, true);
+            Assert.AreEqual(nan == Integer.NaN, false);
+            Assert.AreEqual(zero == ten, false);
+            Assert.AreEqual(two == -two, false);
+            Assert.AreEqual(-thou == thou, false);
+            Assert.AreEqual(inf == -inf, false);
+            Assert.AreEqual(inf == zero, false);
+            Assert.AreEqual(-inf == -thou, false);
+            Assert.AreEqual(nan == zero, false);
+            Assert.AreEqual(nan == one, false);
+            Assert.AreEqual(nan == thou, false);
+            Assert.AreEqual(nan == -two, false);
+            Assert.AreEqual(nan == inf, false);
+            Assert.AreEqual(nan == -inf, false);
+        }
+
+        //[TestMethod]
+        //public void Test()
+        //{
+        //}
+    }
+}
---
+++ InfinitySharp/Integer.cs
@@ -0,0 +1,409 @@
+﻿using System;
+
+//Add modulo and pow
+namespace InfinitySharp
+{
+    public enum OnIntegerOverflow
+    {
+        Throw,
+        Overflow,
+        Infinity
+    }
+
+    public abstract class Integer
+    {
+        protected abstract Integer Add(Integer n);
+        protected abstract Integer Multiply(Integer n);
+        protected abstract Integer Divide(Integer n);
+        protected abstract Integer Negate();
+        protected abstract bool IsEqualTo(Integer n);
+        protected abstract bool IsGreaterThan(Integer n);
+        protected abstract bool IsPositive();
+        protected abstract bool IsZero();
+
+        public static OnIntegerOverflow onOverflow = OnIntegerOverflow.Infinity;
+
+        class FiniteInteger : Integer
+        {
+            int value;
+            public FiniteInteger(int n)
+            {
+                value = n;
+            }
+            public override string ToString()
+            {
+                return value.ToString();
+            }
+            public override int GetHashCode()
+            {
+                return value.GetHashCode();
+            }
+
+            protected override Integer Add(Integer n)
+            {
+                if (n is FiniteInteger fn)
+                    switch (onOverflow)
+                    {
+                        case OnIntegerOverflow.Throw:
+                            try { return new FiniteInteger(checked(value + fn.value)); }
+                            catch (OverflowException) { throw; }
+                        case OnIntegerOverflow.Overflow:
+                            return new FiniteInteger(unchecked(value + fn.value));
+                        case OnIntegerOverflow.Infinity:
+                        default:
+                            try { return new FiniteInteger(checked(value + fn.value)); }
+                            catch (OverflowException) { return value * Infinity; }
+                    }
+
+                else return n.Add(this);
+            }
+
+            protected override Integer Multiply(Integer n)
+            {
+                if (n is FiniteInteger fn)
+                    switch (onOverflow)
+                    {
+                        case OnIntegerOverflow.Throw:
+                            try { return new FiniteInteger(checked(value * fn.value)); }
+                            catch (OverflowException) { throw; }
+                        case OnIntegerOverflow.Overflow:
+                            return new FiniteInteger(unchecked(value * fn.value));
+                        case OnIntegerOverflow.Infinity:
+                        default:
+                            try { return new FiniteInteger(checked(value * fn.value)); }
+                            catch (OverflowException) { return (value > 0 ? 1 : -1) * (fn.value > 0 ? 1 : -1) * Infinity; }
+                    }
+
+                else return n.Multiply(this);
+            }
+
+            protected override Integer Divide(Integer n)
+            {
+                if (n is FiniteInteger fn)
+                    if (IsZero() && fn.IsZero()) return NaN;
+                    else if (IsZero()) return new FiniteInteger(0);
+                    else if (fn.IsZero()) return IsPositive() ? Infinity : -Infinity;
+                    else return new FiniteInteger(value / fn.value);
+                else if (n is Undefined)
+                    return NaN;
+                else if (n is InfiniteInteger)
+                    return new FiniteInteger(0);
+
+                throw new ArgumentException("Unsupported type " + n.GetType() + ". Value: " + n.ToString());
+            }
+
+            protected override Integer Negate()
+            {
+                switch (onOverflow)
+                {
+                    case OnIntegerOverflow.Throw:
+                        try { return new FiniteInteger(checked(-value)); }
+                        catch (OverflowException) { throw; }
+                    case OnIntegerOverflow.Overflow:
+                        return new FiniteInteger(unchecked(-value));
+                    case OnIntegerOverflow.Infinity:
+                    default:
+                        try { return new FiniteInteger(checked(-value)); }
+                        catch (OverflowException) { return (value > 0 ? 1 : -1) * -Infinity; }
+                }
+            }
+
+            protected override bool IsEqualTo(Integer n)
+            {
+                if (n is FiniteInteger fn)
+                    return value == fn.value;
+                return n.IsEqualTo(this);
+            }
+
+            protected override bool IsGreaterThan(Integer n)
+            {
+                if (n is FiniteInteger fn)
+                    return value > fn.value;
+                return n.IsGreaterThan(this);
+            }
+
+            protected override bool IsPositive()
+            {
+                return value > 0;
+            }
+
+            protected override bool IsZero()
+            {
+                return value == 0;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is Integer i)
+                    return IsEqualTo(i);
+
+                return false;
+            }
+        }
+
+        class InfiniteInteger : Integer
+        {
+            public static InfiniteInteger PositiveInfinity;
+            public static InfiniteInteger NegativeInfinity;
+            static InfiniteInteger()
+            {
+                PositiveInfinity = new InfiniteInteger();
+                NegativeInfinity = new InfiniteInteger();
+            }
+            public override string ToString()
+            {
+                return (IsPositive() ? "" : "-") + "Infinity";
+            }
+            public override int GetHashCode()
+            {
+                return IsPositive() ? int.MaxValue : int.MinValue;
+            }
+
+            protected override Integer Add(Integer n)
+            {
+                if (n is FiniteInteger)
+                    return this;
+                else if (n is Undefined)
+                    return NaN;
+                else if (n is InfiniteInteger nI)
+                    if ((IsPositive() && nI.IsPositive()) || (!IsPositive() && !nI.IsPositive()))
+                        return this;
+                    else return NaN;
+
+                throw new ArgumentException("Unsupported type " + n.GetType() + ". Value: " + n.ToString());
+            }
+
+            protected override Integer Multiply(Integer n)
+            {
+                if (n is FiniteInteger)
+                    if (n.IsZero())
+                        return NaN;
+                    else return n.IsPositive() ? this : -this;
+                else if (n is Undefined)
+                    return NaN;
+                else if (n is InfiniteInteger nI)
+                    return nI.IsPositive() ? this : -this;
+
+                throw new ArgumentException("Unsupported type " + n.GetType() + ". Value: " + n.ToString());
+            }
+
+            protected override Integer Divide(Integer n)
+            {
+                if (n is FiniteInteger)
+                    return (n.IsZero() || n.IsPositive()) ? this : -this;
+                else if (n is Undefined || n is InfiniteInteger)
+                    return NaN;
+
+                throw new ArgumentException("Unsupported type " + n.GetType() + ". Value: " + n.ToString());
+            }
+
+            protected override Integer Negate()
+            {
+                return Equals(PositiveInfinity) ? NegativeInfinity : PositiveInfinity;
+            }
+
+            protected override bool IsEqualTo(Integer n)
+            {
+                if (n is FiniteInteger || n is Undefined)
+                    return false;
+                else if (n is InfiniteInteger nI)
+                    return (IsPositive() && nI.IsPositive()) || (!IsPositive() && !nI.IsPositive());
+
+                throw new ArgumentException("Unsupported type " + n.GetType() + ". Value: " + n.ToString());
+            }
+
+            protected override bool IsGreaterThan(Integer n)
+            {
+                if (n is FiniteInteger)
+                    return IsPositive();
+                else if (n is Undefined)
+                    return false;
+                else if (n is InfiniteInteger nI)
+                    return IsPositive() && !nI.IsPositive();
+
+                throw new ArgumentException("Unsupported type " + n.GetType() + ". Value: " + n.ToString());
+            }
+
+            protected override bool IsPositive()
+            {
+                return Equals(PositiveInfinity);
+            }
+
+            protected override bool IsZero()
+            {
+                return false;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return (object)this == obj;
+            }
+        }
+
+        class Undefined : Integer
+        {
+            public static Undefined UndefinedNumber;
+            static Undefined()
+            {
+                UndefinedNumber = new Undefined();
+            }
+            public override string ToString()
+            {
+                return "NaN";
+            }
+            public override int GetHashCode()
+            {
+                return 0;
+            }
+
+            protected override Integer Add(Integer n)
+            {
+                return UndefinedNumber;
+            }
+
+            protected override Integer Multiply(Integer n)
+            {
+                return UndefinedNumber;
+            }
+
+            protected override Integer Divide(Integer n)
+            {
+                return UndefinedNumber;
+            }
+
+            protected override Integer Negate()
+            {
+                return UndefinedNumber;
+            }
+
+            protected override bool IsEqualTo(Integer n)
+            {
+                return false;
+            }
+
+            protected override bool IsGreaterThan(Integer n)
+            {
+                return false;
+            }
+
+            protected override bool IsPositive()
+            {
+                return false;
+            }
+
+            protected override bool IsZero()
+            {
+                return false;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return (object)this == obj;
+            }
+        }
+
+        public static implicit operator Integer(int n)
+        {
+            return new FiniteInteger(n);
+        }
+
+        public static Integer operator -(Integer n1)
+        {
+            return n1.Negate();
+        }
+
+        public static Integer operator +(Integer n1, Integer n2)
+        {
+            return n1.Add(n2);
+        }
+
+        public static Integer operator -(Integer n1, Integer n2)
+        {
+            return n1.Add(-n2);
+        }
+
+        public static Integer operator *(Integer n1, Integer n2)
+        {
+            return n1.Multiply(n2);
+        }
+
+        public static Integer operator /(Integer n1, Integer n2)
+        {
+            return n1.Divide(n2);
+        }
+
+        public static bool operator ==(Integer n1, Integer n2)
+        {
+            return n1.IsEqualTo(n2);
+        }
+
+        public static bool operator !=(Integer n1, Integer n2)
+        {
+            return !n1.IsEqualTo(n2);
+        }
+
+        public static bool operator >(Integer n1, Integer n2)
+        {
+            return n1.IsGreaterThan(n2);
+        }
+
+        public static bool operator <(Integer n1, Integer n2)
+        {
+            return n2.IsGreaterThan(n1);
+        }
+
+        public static Integer Pow(Integer x, Integer y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Integer Infinity
+        {
+            get
+            {
+                return InfiniteInteger.PositiveInfinity;
+            }
+        }
+
+        public static Integer NaN
+        {
+            get
+            {
+                return Undefined.UndefinedNumber;
+            }
+        }
+
+        public bool IsFinite()
+        {
+            return this is FiniteInteger;
+        }
+
+        public bool IsInfinite()
+        {
+            return this is InfiniteInteger;
+        }
+
+        public bool IsNaN()
+        {
+            return this is Undefined;
+        }
+
+        public static Integer Finite(int n)
+        {
+            return new FiniteInteger(n);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Integer i)
+                return IsEqualTo(i);
+
+            return false;
+        }
+    }
+}
